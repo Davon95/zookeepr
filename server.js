7d7652a69fdf7bc15f3dfa512c15fc7979e90fbc
Davon <dkwebb95@gmail.com>
2022-07-11 19:58:58 -0400

    added POST and validation to current data server

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals')
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
@@ -23,6 +30,19 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
         
+});
+
+app.post('/api/animals', (req, res) => {
+    // Set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // If any data in req.body is incorrect, send 400 error back
+    if (!validateAnima(req.body)) {
+        res.status(400).send('The Animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    } 
 });
 
 function filterByQuery(query, animalsArray) {
@@ -58,4 +78,31 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync (
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
+}
+
+function validateAnima(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+        return false;
+    }
+    return true;
 }
